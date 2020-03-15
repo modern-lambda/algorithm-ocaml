b3fa59f1d5f1bfcf028fb7929c28b463803a6673
CanftIn <wwc7033@gmail.com>
2020-03-15 22:45:41 +0800

    update stack.ml

---
+++ stack.ml
@@ -0,0 +1,28 @@
+type 'a t =
+  | Empty
+  | Item of 'a * 'a t
+
+let rec push stack item = match stack with
+  | Empty -> Item (item, Empty)
+  | tail -> Item (item, tail)
+
+let pop = function
+  | Empty -> None
+  | Item (head, rest) -> Some (head, rest)
+
+let peek = function
+  | Empty -> None
+  | Item (head, _) -> Some head
+
+let rec stack_of_list = function
+  | [] -> Empty
+  | head :: tail -> Item (head, stack_of_list tail)
+
+let rec list_of_stack = function
+  | Empty -> []
+  | Item (head, rest) -> head :: list_of_stack rest
+
+let rec fmap f = function
+  | Empty -> Empty
+  | Item (head, tail) -> Item (f head, fmap f tail)
+
