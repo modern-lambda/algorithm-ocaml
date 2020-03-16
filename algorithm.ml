f15060d21143bb2a176d4544810c7d1db11084e3
CanftIn <wwc7033@gmail.com>
2020-03-16 16:14:56 +0800

    update merge sort

--- algorithm.ml
+++ algorithm.ml
@@ -4,11 +4,6 @@ let rec factorial a =
   | 1 -> 1
   | _ -> a * factorial (a - 1)
 
-(* isvowel : int -> int *)
-let rec isvowel c =
-  c = 'a' || c = 'e' || c = 'i' || c = 'o' || c = 'u'
-
-
 let rec length l =
   match l with
   | [] -> 0
@@ -40,4 +35,25 @@ let rec sort l =
   match l with
   | [] -> []
   | h :: t -> sort_insert h (sort t)
+
+let rec merge cmp x y =
+  match (x, y) with
+  | ([], _) -> y
+  | (_, []) -> x
+  | (h1 :: t1, h2 :: t2) ->
+    if cmp h2 h1
+    then h1 :: (merge cmp t1 y)
+    else h2 :: (merge cmp x t2)
+
+and split x y z =
+  match x with
+  | [] -> (y, z)
+  | x :: resto -> split resto z (x :: y)
+
+and mergesort cmp x =
+  match x with
+  | ([] | _ :: []) -> x
+  | _ -> let (pri, seg) = split x [] []
+    in (merge cmp (mergesort cmp pri) (mergesort cmp seg))
+
 
